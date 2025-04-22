cfbe85473455bae7d28eb1206b45e0aa0364b5e0
Pranab <napikpranab@gmail.com>
2025-04-22 20:58:54 +0800

    Added Node Modules

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
         <div className="container">
-          <Link className="navbar-brand" to="/">JobPortal</Link>
+          <Link className="navbar-brand" to="/">JobConnect</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="navbar-toggler-icon"></span>
           </button>
